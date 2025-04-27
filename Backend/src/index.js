c72a699ebe62a5fd6da50ae1ae64e901bfe919b2
Pavan Kumar <siriyalapavankumar@gmail.com>
2025-04-27 23:54:09 +0530

    register_controller

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,10 +1,18 @@
-import express from "express"
+import express from "express";
 import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
-const app =  express();
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
 
+app.get("/", (req, res) => {
+  res.send("hello ");
+});
 
-app.listen(process.env.PORT, ( )=>{
-  console.log("server is up and runing on 3000") 
-}) 
+app.use("/api/v1/auth", authRoutes);
+app.listen(process.env.PORT, () => {
+  console.log(`server is up and runing on  ${process.env.PORT}`);
+});
