54986b4fc9943ee3356a0dfd025d32fedc476ed4
Pavan Kumar <siriyalapavankumar@gmail.com>
2025-05-01 18:55:22 +0530

    problem -> ById,getAll,update,delete controllers

--- Backend/src/controllers/problem.controller.js
+++ Backend/src/controllers/problem.controller.js
@@ -69,8 +69,115 @@ export const createProblem = async (req, res) => {
     });
   }
 };
-export const getAllProblems = async (req, res) => {};
-export const getProbelmById = async (req, res) => {};
-export const updateProblem = async (req, res) => {};
-export const deleteProblem = async (req, res) => {};
+export const getAllProblems = async (req, res) => {
+  try {
+    const problems = await db.problem.findMany();
+    if (!problems) {
+      res.status(404).json({
+        error: "No problems found",
+      });
+    }
+    res.status(200).json({
+      sucess: true,
+      message: "problems Fetched Successfully",
+      problem: problems,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      error: "Error While Fetching Problems",
+    });
+  }
+};
+export const getProbelmById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const problem = await db.problem.findUnique({
+      where: {
+        id,
+      },
+    });
+    if (!problem) {
+      return res.status(404).json({ error: "Problem not found." });
+    }
+    res.status(200).json({
+      sucess: true,
+      message: "ProblemId Successfully",
+      problem,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      error: "Error While Fetching Problem by id",
+    });
+  }
+};
+export const updateProblem = async (req, res) => {
+  const id = req.params;
+  const {
+    title,
+    description,
+    difficulty,
+    tags,
+    examples,
+    constraints,
+    testcases,
+    codeSnippets,
+    referenceSolution,
+  } = req.body;
+  try {
+    const existingProblem = await db.problem.findUnique({ where: { id } });
+    if (!existingProblem) {
+      return res.status(404).json({ error: "Problem not found" });
+    }
+    const updatedProblem = await db.problem.update({
+      where: {
+        id,
+      },
+      data: {
+        title,
+        description,
+        difficulty,
+        tags,
+        examples,
+        constraints,
+        testcases,
+        codeSnippets,
+        referenceSolution,
+        userId: req.user.id,
+      },
+    });
+    return res.status(200).json({
+      success: true,
+      message: "Problem updated successfully",
+      problem: updatedProblem,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      error: "Error while updating the problem",
+    });
+  }
+};
+
+export const deleteProblem = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const problem = await db.problem.findUnique({ where: { id } });
+    if (!problem) {
+      return res.status(404).json({ error: "Problem Not found" });
+    }
+    await db.problem.delete({ where: { id } });
+
+    res.status(200).json({
+      success: true,
+      message: "Problem deleted Successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      error: "Error While deleting the problem",
+    });
+  }
+};
 export const getAllProblemSolvedByTheUser = async (req, res) => {};
--- Backend/src/middlewares/authMiddleware.js
+++ Backend/src/middlewares/authMiddleware.js
@@ -1,4 +1,3 @@
-import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { db } from "../libs/db.js";
 
